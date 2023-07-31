a9d6dac369d90a9ae2c46bcdaab5a5ee54047fd4
chanon-mike <zzmikezz8520@gmail.com>
2023-07-31 17:02:49 +0900

    Fix settings

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   parserOptions: {
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: './frontend/tsconfig.json',
     ecmaFeatures: { jsx: true },
   },
   rules: {
