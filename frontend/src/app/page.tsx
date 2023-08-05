c42e2ed5a22a2f02eb3263dbe24b2626fbaf4b79
chanon-mike <zzmikezz8520@gmail.com>
2023-08-05 15:28:05 +0900

    Add homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,36 @@
+import { getSession } from '@auth0/nextjs-auth0';
+import { ArrowForward } from '@mui/icons-material';
+import { Container, Box, Typography, Button, Link } from '@mui/material';
+import { redirect } from 'next/navigation';
+
 import * as React from 'react';
 
 
-const Home = () => {
+const Home = async () => {
+  const session = await getSession();
+
   return (
-    <></>
+    <Container>
+      {!session ? (
+        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="80vh">
+          <Typography variant="h2" gutterBottom>
+            内定Journeyへようこそ!
+          </Typography>
+          <Typography variant="h6" paragraph>
+            ログインして、内定までの道を管理しましょう！
+          </Typography>
+          <Box mt={2}>
+            <Link href="/api/auth/login" underline="none">
+              <Button variant="outlined" color="secondary" endIcon={<ArrowForward />}>
+                サインイン
+              </Button>
+            </Link>
+          </Box>
+        </Box>) :
+        // TODO: Add user statistics instead of redirecting to intern page
+        redirect('/intern')
+      }
+    </Container>
   );
 };
 
