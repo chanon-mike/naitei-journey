a459b8824763993dd4bda7aa65de4051f53e707b
yutam29 <yuki1129taniguchi@gmail.com>
2023-08-10 00:25:41 +0900

    fix a problem

--- frontend/src/components/popup/RankingSelector.tsx
+++ frontend/src/components/popup/RankingSelector.tsx
@@ -8,7 +8,6 @@ import * as React from 'react';
 interface RankingSelectorProps {
   rank: string | null;
   onRankChange: (rank: string) => void;
-  value: string;
 }
 
 const RankingSelector: React.FC<RankingSelectorProps> = ({ rank, onRankChange }) => {
